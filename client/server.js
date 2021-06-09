336096cef1b026e56511e5a973f9acbd759dc06a
uriel <ualvarez95@gmail.com>
2021-06-09 18:37:06 -0300

    https to http

--- client/server.js
+++ client/server.js
@@ -1,15 +1,19 @@
-//Install express server
-const express = require('express');
-const path = require('path');
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
 
+const express = require('express');
 const app = express();
 
-// Serve only the static files form the dist directory
+app.use(requireHTTPS);
 app.use(express.static('./dist/client'));
 
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/client/'}),
 );
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8081);
